966d9a57813b66e90a5b8a9fb82b081e75ad5b32
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-02-07 10:22:43 -0500

    added an image loader for github pages

--- package/next.config.ts
+++ package/next.config.ts
@@ -2,12 +2,19 @@ import type { NextConfig } from "next";
 
 
 
+
 const isProd = process.env.GH_PAGES_BUILD;
+const basePath = isProd ? "/portfolio-2026" : "";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: isProd ? "/portfolio-2026" : "",
+    basePath: basePath,
+    env: {
+        NEXT_PUBLIC_BASE_PATH: basePath,
+    },
     images: {
+        loader: "custom",
+        loaderFile: "./src/image-loader.ts",
         unoptimized: true,
     },
     trailingSlash: true,

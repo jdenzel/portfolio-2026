640bd827c7443a084e87df372a714e1c26b81704
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-02-07 10:38:02 -0500

    Changed brand-slider to use nextjs images

--- package/src/app/components/shadcn-space/blocks/hero-01/brand-slider.tsx
+++ package/src/app/components/shadcn-space/blocks/hero-01/brand-slider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Marquee } from "@/app/components/shadcn-space/animations/marquee";
 import { motion } from "motion/react";
+import Image from "next/image";
 
 export interface BrandList {
   image: string;
@@ -24,16 +25,20 @@ function BrandSlider({ brandList }: { brandList: BrandList[] }) {
               <div>
                 <Marquee pauseOnHover className="[--duration:20s] p-0">
                   {brandList.map((brand, index) => (
-                    <div key={index}>
-                      <img
+                    <div key={index} className="flex items-center">
+                      <Image
                         src={brand.image}
                         alt={brand.name}
-                        className="w-36 h-8 mr-6 lg:mr-20 dark:hidden"
+                        width={144}
+                        height={32}
+                        className="w-36 h-8 mr-6 lg:mr-20 dark:hidden object-contain"
                       />
-                      <img
+                      <Image
                         src={brand.lightimg}
                         alt={brand.name}
-                        className="hidden dark:block w-36 h-8 mr-12 lg:mr-20"
+                        width={144}
+                        height={32}
+                        className="hidden dark:block w-36 h-8 mr-12 lg:mr-20 object-contain"
                       />
                     </div>
                   ))}

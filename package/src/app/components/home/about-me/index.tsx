ba589d18a75c556ed52d4b8384b338c73e48a832
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-06-07 00:38:22 -0400

    Updated resume and about me section

--- package/src/app/components/home/about-me/index.tsx
+++ package/src/app/components/home/about-me/index.tsx
@@ -19,7 +19,7 @@ const AboutMe = () => {
 
         <div className="relative z-10">
           <div className="container">
-            <MotionAnimation delay={.1}>
+            <MotionAnimation delay={0.1}>
               <div className="flex items-center justify-between gap-2 border-b border-primary pb-7">
                 <h2>About Me</h2>
                 <p className="text-xl text-primary">( 01 )</p>
@@ -44,37 +44,41 @@ const AboutMe = () => {
               <div className="w-full lg:max-w-2xl flex-1">
                 <MotionAnimation delay={0.3}>
                   <p className="text-lg">
-                    Student studying computer science at Towson University with a
-                    strong interest in web development. Experienced in building
-                    modern web applications using Next.js and Supabase, with a
-                    focus on clean architecture and real-world usability. I
-                    currently serve as IT Lead at my local church where I manage
-                    the Network Infrastructure and AV technology, as well as serve
-                    as a board member of Infinite Group where I build applications
-                    for our basketball league and solve technical problems.
-                    Outside of tech, I enjoy shooting hoops in basketball, playing
-                    story-driven games like Cyberpunk 2077 and The Witcher 3, and
-                    mixing audio.
+                    Hi, my name is Denzel. I'm a recent graduate from Towson
+                    University and an aspiring Full-Stack Software Engineer with
+                    a passion for tackling complex architectural challenges and
+                    engineering software solutions. I'm proficient in developing
+                    with modern tech tools including Next.js, React.js, Node.js,
+                    Typescript, and Tailwind CSS, coupled with robust database
+                    management using Supabase. I'm currently the Technical Lead
+                    at Infinite Group where I build the digital infrastructure
+                    needed to empower our local community through sports. I also
+                    lead the tech team at River of Life International Christian
+                    Fellowship, using my technical expertise to foster
+                    connection and bring people closer to Christ. My personal
+                    goal is to use all the skills that I learn and the
+                    experience I gain, to help and serve my community. 1st Peter
+                    4:10 As each has received a gift, use it to serve one
+                    another, as good stewards of God's varied grace
                   </p>
                 </MotionAnimation>
 
-                <MotionAnimation delay={.4}>
+                <MotionAnimation delay={0.4}>
                   <div className="flex justify-around pt-10 xl:pt-16 pb-2  border-b border-primary">
                     <Laugh className="bg-red-500/10 text-red-500 p-2 rounded-full size-15" />
                     <Gamepad2 className="bg-green-500/10 text-green-500 p-2 rounded-full size-15" />
                     <Music className="bg-blue-500/10 text-blue-500 p-2 rounded-full size-15" />
                   </div>
                 </MotionAnimation>
-
               </div>
             </div>
           </div>
         </div>
-        <MotionAnimation delay={.5}>
+        <MotionAnimation delay={0.5}>
           <Gallery />
         </MotionAnimation>
       </div>
-    </section >
+    </section>
   );
 };
 
